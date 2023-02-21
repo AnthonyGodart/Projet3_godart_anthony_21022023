011d7c7281bf336469599f41829a0377f6f102f2
Anthony GODART <107860091+AnthonyGodart@users.noreply.github.com>
2023-02-21 10:07:34 +0100

    update to current 1.2

--- FrontEnd/front-app.js
+++ FrontEnd/front-app.js
@@ -26,9 +26,20 @@ generateWorkSheet(workList);
 // Créer les filtres en fonction de la catégorie
 
 const filterButtons = document.querySelectorAll(".filter")
+const workIds = workList.map(id => id.category.id)
+
+
+
 for(let button of filterButtons){
     button.addEventListener('click', () => {
         //Ici le code pour filtrer les <figure> par catégorie (data-id)
-        console.log("Ca fonctionne !")
+        let clickedButtonId = button.dataset.id
+        for ( let i = workIds.length - 1; i >= 0; i--){
+            if(workIds[i].id !== clickedButtonId){
+                workIds.splice(i, 1)
+            }
+
+        console.log(workIds)
+        }
     });
 }
